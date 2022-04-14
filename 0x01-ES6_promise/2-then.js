a43ec6eeee67f74de736a32279a870bd48c3c945
lahincapie <alejandra612@hotmail.com>
2022-04-13 22:47:02 -0500

    2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      return (
+        {
+          status: 200,
+          body: 'Success',
+        }
+      );
+    })
+    .catch(() => {
+      return (new Error());
+    })
+    .finally(() => {
+      return (console.log('Got a response from the API'));
+    })
+}
