5b4d2228ae875c38a9d40930f257d0d614628858
lahincapie <alejandra612@hotmail.com>
2022-04-14 21:01:47 -0500

    3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,28 @@
 export default class Currency {
-    constructor(code, name){
-      this._code = code;
-      this._name = name;
+  constructor(code, name){
+    this._code = code;
+    this._name = name;
+  }
+  get code() {
+    return this._code;
+  }
+  set code(value) {
+    if (value !== 'string') {
+      throw new TypeError('Code must be a string')
     }
-    get code() {
-      return this._code;
+    this._code = value;
+  }
+  get name() {
+    return this._name
+  }
+  set name(value) {
+    if (value !== 'string') {
+      throw new TypeError('Name must be a string')
     }
-    set code(value) {
-      if (value !== 'string') {
-        throw new TypeError('Code must be a string')
-      }
-      this._code = value;
-    }
-    get name() {
-      return this._name
-    }
-    set name(value) {
-      if (value !== 'string') {
-        throw new TypeError('Name must be a string')
-      }
-      this._name = value;
-    }
+    this._name = value;
+  }
 
-    displayFullCurrency() {
-      return(`${this._name} (${this._code})`)
-    }
+  displayFullCurrency() {
+    return(`${this._name} (${this._code})`)
+  }
 }
