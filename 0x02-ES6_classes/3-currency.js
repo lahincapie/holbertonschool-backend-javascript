d26bc8c52e604f71931d3aea24d1461de90fedc8
lahincapie <alejandra612@hotmail.com>
2022-04-14 21:04:07 -0500

    3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -8,21 +8,21 @@ export default class Currency {
   }
   set code(value) {
     if (value !== 'string') {
-      throw new TypeError('Code must be a string')
+      throw new TypeError('Code must be a string');
     }
     this._code = value;
   }
   get name() {
-    return this._name
+    return this._name;
   }
   set name(value) {
     if (value !== 'string') {
-      throw new TypeError('Name must be a string')
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
 
   displayFullCurrency() {
-    return(`${this._name} (${this._code})`)
+    return(`${this._name} (${this._code})`);
   }
 }
