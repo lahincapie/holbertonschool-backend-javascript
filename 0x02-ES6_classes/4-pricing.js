a870a16b3ae2ca4cfe0a4d5777b7f723d1f2b1f9
lahincapie <alejandra612@hotmail.com>
2022-04-14 21:37:38 -0500

    4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import Currency from './3-currency'
+import Currency from './3-currency.js'
 
 export default class Pricing {
   constructor(amount, currency) {
@@ -10,29 +10,24 @@ export default class Pricing {
   get amount() {
     return this._amount;
   }
+
   set amount(value) {
-    if (value !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
     this._amount = value;
   }
+
   get currency() {
     return this._currency;
   }
+
   set currency(value) {
-    if (!(value instanceof Currency)) {
-      throw TypeError('Currency must be a type of Currency');
-    }
     this._currency = value;
   }
 
   displayFullPrice() {
-    return (`${this._amount} ${this._currency.displayFullCurrency()}`)
+    return (`${this._amount} ${this.currency.name} (${this.currency.code})`)
   }
+
   static convertPrice(amount, conversionRate) {
-    if (amount !== 'number' || conversionRate !== 'number') {
-      throw new TypeError('Amount and conversionRate must be a number');
-    }
     return (amount * conversionRate);
   }
 }
