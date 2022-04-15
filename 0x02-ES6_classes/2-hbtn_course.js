71124a1c7292f335892f4b88a262489b078aab67
lahincapie <alejandra612@hotmail.com>
2022-04-14 19:32:20 -0500

    2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === "string") {
+      this._name = name;
+    }
+    else {
+      throw new TypeError("Name must be a string");
+    }
+    if (typeof length === "number") {
+      this._length = length;
+    }
+    else {
+      throw new TypeError("Length must be a number");
+    }
+    students.forEach((student) => {
+      if (typeof student === 'string') {
+        this._students = students;
+      }
+      else {
+        throw new TypeError("Student must be a array of string")
+      }
+    });
+  }
+  get name() {
+    return this._name
+  }
+  set name(value) {
+    this._name = value;
+  }
+  get length() {
+    return this._length
+  }
+  set length(value) {
+    this._length = value;
+  }
+  get students() {
+    return this._students
+  }
+  set students(value) {
+    this._students = value;
+  }
+}
