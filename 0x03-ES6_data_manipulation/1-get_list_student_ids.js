07d2805bc76cd75b067415ba29e3b95959cc4d95
lahincapie <alejandra612@hotmail.com>
2022-04-19 11:05:52 -0500

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,8 @@
-const getListStudentIds = (myArray) => {
-    if (!(Array.isArray(myArray)))
-        return myArray = [];
+const getListStudentIds = (myArray = []) => {
+  if (Array.isArray(myArray)) {
     return myArray.map((studentId) => studentId.id);
-}
+  }
+  return myArray;
+};
 
 export default getListStudentIds;
